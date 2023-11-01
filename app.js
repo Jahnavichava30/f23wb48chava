cc0dbdfed9d7fa6487ad6ad8d5c65046f1d703a8
Jahnavichava30 <142965078+Jahnavichava30@users.noreply.github.com>
2023-11-01 14:26:09 -0500

    added choose js and pug

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var HatsRouter = require('./routes/Hats');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
+
 var app = express();
 
 // view engine setup
@@ -24,6 +26,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Hats', HatsRouter);
 app.use('/board',boardRouter);
+app.use('/choose',chooseRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
